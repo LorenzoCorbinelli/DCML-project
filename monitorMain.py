db057d3ffbff32093253377c26f539cfbb07a6ac
Lorenzo Corbinelli <lore.corbi01@gmail.com>
2023-10-06 18:40:12 +0200

    Added saving to csv file

--- monitorMain.py
+++ monitorMain.py
@@ -1,16 +1,35 @@
+import os.path
 import time
 import psutil
+import csv
 
-# dict_test = {"user": "Lorenzo", "pass": "dcml"}
-# cpu_t_p = psutil.cpu_times_percent(interval=1, percpu=False)._asdict()
-# disk_usage = psutil.disk_usage('/')._asdict()
-# cpu_t_p.update(disk_usage)
 
-n = 10
-while n > 0:
-    start_time = time.time_ns()
-    mem = psutil.virtual_memory()._asdict()
-    exec_time = time.time_ns() - start_time
-    time.sleep(1 - exec_time * pow(10,-9))
-    print(mem)
-    n -= 1
+def monitor(max_obs, csv_filename):
+    '''
+    Function for monitoring
+    :param max_obs: maximum number of observations
+    :param csv_filename: name of the output file csv
+    :return: nothing
+    '''
+
+    if os.path.exists(csv_filename):
+        os.remove(csv_filename)
+    n = 0
+    while n < max_obs:
+        start_time = time.time_ns()
+        cpu_t_p = psutil.cpu_times_percent(interval=0.1, percpu=False)._asdict()
+        mem = psutil.virtual_memory()._asdict()
+        cpu_t_p.update(mem)
+        with open(csv_filename, "a", newline="") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=cpu_t_p.keys())
+            if n == 0:
+                writer.writeheader()
+            writer.writerow(cpu_t_p)
+        print(cpu_t_p)
+        exec_time = time.time_ns() - start_time
+        time.sleep(1 - exec_time / 1e+9)
+        n += 1
+
+
+if __name__ == "__main__":
+    monitor(10, "data.csv")
