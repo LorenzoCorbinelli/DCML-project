27094dd01c42d7ce82eff769010e28bb9388cbf2
Lorenzo Corbinelli <lore.corbi01@gmail.com>
2023-10-06 10:25:50 +0200

    Updated monitorMain

--- monitorMain.py
+++ monitorMain.py
@@ -1,8 +1,16 @@
+import time
 import psutil
 
-cpu_t_p = psutil.cpu_times_percent(interval=1, percpu=False)._asdict()
-disk_usage = psutil.disk_usage('/')._asdict()
+# dict_test = {"user": "Lorenzo", "pass": "dcml"}
+# cpu_t_p = psutil.cpu_times_percent(interval=1, percpu=False)._asdict()
+# disk_usage = psutil.disk_usage('/')._asdict()
+# cpu_t_p.update(disk_usage)
 
-cpu_t_p.update(disk_usage)
-
-dict_test = {"user": "Lorenzo", "pass": "dcml"}
+n = 10
+while n > 0:
+    start_time = time.time_ns()
+    mem = psutil.virtual_memory()._asdict()
+    exec_time = time.time_ns() - start_time
+    time.sleep(1 - exec_time * pow(10,-9))
+    print(mem)
+    n -= 1
