1a23a3124f0a32bbe84b0a5588c98c67bba288e0
Lorenzo Corbinelli <lore.corbi01@gmail.com>
2023-12-19 12:29:55 +0100

    Added timestamp and label column on the data csv file

--- monitorMain.py
+++ monitorMain.py
@@ -2,6 +2,7 @@ import os.path
 import time
 import psutil
 import csv
+import pandas
 
 
 def monitor(max_obs, csv_filename):
@@ -17,7 +18,7 @@ def monitor(max_obs, csv_filename):
     n = 0
     while n < max_obs:
         start_time = time.time_ns()
-        data = {}
+        data = {"timestamp": round(start_time / 1e+6)}
 
         # Elements to monitoring
         cpu = CPUMonitor()
@@ -32,10 +33,28 @@ def monitor(max_obs, csv_filename):
             if n == 0:
                 writer.writeheader()
             writer.writerow(data)
-        print(round(start_time / 1e+6), data)
+        print(data)
         exec_time = time.time_ns() - start_time
         time.sleep(1 - exec_time / 1e+9)
         n += 1
+
+    # add on csv the label column
+    data_frame = pandas.read_csv("data.csv", sep=',')
+    CPU_injection = pandas.read_csv("CPU_injection.csv", sep=',')
+    memory_injection = pandas.read_csv("memory_injection.csv", sep=',')
+    disk_injection = pandas.read_csv("disk_injection.csv", sep=',')
+
+    label = []
+    for timestamp in data_frame["timestamp"]:
+        if (CPU_injection["startTime"][0] <= timestamp <= CPU_injection["endTime"][0] or
+                memory_injection["startTime"][0] <= timestamp <= memory_injection["endTime"][0] or
+                disk_injection["startTime"][0] <= timestamp <= disk_injection["endTime"][0]):
+            label.append(1)  # during the injection -> anomaly
+        else:
+            label.append(0)  # normal
+
+    data_frame["label"] = label
+    data_frame.to_csv("data.csv", index=False)
 
 
 def CPUMonitor():
@@ -51,4 +70,4 @@ def diskMonitor():
 
 
 if __name__ == "__main__":
-    monitor(15, "data.csv")
+    monitor(20, "data.csv")
