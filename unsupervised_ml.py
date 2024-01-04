af573abdbe27e46ae6868ac19d5e6ce9f1973216
Lorenzo Corbinelli <lore.corbi01@gmail.com>
2024-01-04 16:44:22 +0100

    Changed contamination parameter

--- unsupervised_ml.py
+++ unsupervised_ml.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
 
     feat_train, feat_test, lab_train, lab_test = train_test_split(features, label, test_size=0.5, shuffle=False)
 
-    classifiers = [HBOS(contamination=0.096), ABOD(contamination=0.096), COPOD(contamination=0.096)]
+    classifiers = [HBOS(contamination=0.192), ABOD(contamination=0.192), COPOD(contamination=0.192)]
 
     for cl in classifiers:
         start_train = current_ms()
